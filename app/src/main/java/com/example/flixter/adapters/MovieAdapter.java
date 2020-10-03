c6a67931020f39c7dc053d5ffbb5a0cd79908319
ysajjan81 <ysajjan81@gmail.com>
2020-10-02 22:03:50 -0700

    assignment 2

--- app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
@@ -1,20 +1,27 @@
 package com.example.flixter.adapters;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Parcelable;
 import android.telecom.Conference;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixter.DetailActivity;
 import com.example.flixter.R;
 import com.example.flixter.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -52,6 +59,7 @@ public  class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>
     }
 
     public  class ViewHolder extends RecyclerView.ViewHolder {
+        RelativeLayout container ;
         TextView tvTitle;
         TextView tvOverview ;
         ImageView ivPoster;
@@ -61,9 +69,10 @@ public  class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText((movie.getTitle()));
             tvOverview.setText(movie.getOverview());
             String imageUrl ;
@@ -75,6 +84,17 @@ public  class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>
                 imageUrl = movie.getPosterPath();
             }
             Glide.with(context).load(imageUrl).into(ivPoster);
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+//                    Toast.makeText(context, movie.getTitle(), Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("title",movie.getTitle());
+//                    i.putExtra("movie",movie);
+                        i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/example/flixter/models/Movie.java
+++ app/src/main/java/com/example/flixter/models/Movie.java
@@ -5,22 +5,29 @@ import android.content.Intent;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Parcel
 public class Movie {
+    String movieId ;
     String posterPath ;
     String backdropPath;
     String title;
     String overview ;
+    float rating;
 
+    public Movie(){
+
+    }
     public  Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString(("poster_path"));
         title = jsonObject.getString("title");
         overview = jsonObject.getString(("overview"));
-
+        rating = (float)jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getString("id");
     }
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException{
         List<Movie> movies = new ArrayList<>();
@@ -44,5 +51,13 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public String getMovieId() {
+        return movieId;
     }
 }
